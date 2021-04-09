483557a10876d48233624615cc575b778beda396
antony <antony.inga@unas.edu.pe>
2021-04-09 10:11:57 -0500

    cambio de colores a toda la aplicacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,12 +8,6 @@ import { Component, OnInit } from "@angular/core";
 export class AppComponent implements OnInit {
   ngOnInit() {
     // document.getElementsByTagName("html")[0].setAttribute("dir", "rtl");
-    document.body.setAttribute("data-sidebar", "light");
-    document.body.setAttribute("data-topbar", "dark");
-    document.body.removeAttribute("data-sidebar-size");
-    document.body.removeAttribute("data-layout-size");
-    document.body.removeAttribute("data-keep-enlarged");
-    document.body.classList.remove("vertical-collpsed");
-    document.body.removeAttribute("data-layout-scrollable");
+  
   }
 }
